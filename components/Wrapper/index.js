559f9f22924871bfd4d466a38d3a02c6a0bc4e57
anatoliy.zolotuhin <zolotukhinwebpr@mail.ur>
2019-05-28 15:06:39 +0700

    delete: dep

--- components/Wrapper/index.js
+++ components/Wrapper/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-
 import { styles } from './styles'
 
 const Wrapper = ({ classes, children }) => (
